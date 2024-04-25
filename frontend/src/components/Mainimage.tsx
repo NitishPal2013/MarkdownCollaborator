ae1a89ac18421a8e2d299cd594a525ec9f9af27a
npccodes <npccodes@gmail.com>
2024-04-25 19:02:16 +0530

    Added MainImage.tsx in components folder

---
+++ frontend/src/components/Mainimage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+const Mainimage: React.FC = () => {
+  return (
+    <>
+    <img src="https://th.bing.com/th/id/OIG3.vtrcsBeEHH4Y6bwt9Z51?w=1024&h=1024&rs=1&pid=ImgDetMain" alt="main_image" id="main_image"/>
+    <Typography variant="h3">Markdown Collaborator</Typography>
+    </>
+  )
+}
+
+export default Mainimage
